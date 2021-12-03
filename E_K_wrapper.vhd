2367a8c948493b6fc6e36c9a7200107114d19e02
aadam <adirie2@gmu.edu>
2021-12-03 01:13:56 -0500

    Verified Skinny-128-384+ used in ROM N and tested with TB. Created Controller and Wrapper to make processing easier for main controller

---
+++ E_K_wrapper.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/30/2021 01:40:31 AM
+-- Design Name: 
+-- Module Name: E_K_wrapper - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity E_K_wrapper is
+--  Port ( );
+end E_K_wrapper;
+
+architecture Behavioral of E_K_wrapper is
+
+begin
+
+
+end Behavioral;
